47165a606384810eab6ab6b6017bccb34c687724
Anupam Samanta <anupam96786@gmail.com>
2023-05-16 19:30:34 +0530

    Audio recording functionality added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RecordAudioComponent} from "./components/record-audio/record-audio.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RecordAudioComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/record-audio/record-audio.component.ts
+++ src/app/components/record-audio/record-audio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-record-audio',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./record-audio.component.scss']
 })
 export class RecordAudioComponent {
+  mediaRecorder!: MediaRecorder;
+  audioChunks: Blob[] = [];
+  audioUrl?: string;
 
+  constructor() {
+    navigator.mediaDevices.getUserMedia({audio: true}).then(stream => {
+      this.mediaRecorder = new MediaRecorder(stream, {
+        mimeType: 'audio/webm',
+        audioBitsPerSecond: 22050,
+      });
+
+      this.mediaRecorder.addEventListener("dataavailable", event => {
+        console.log('yo')
+        this.audioChunks.push(event.data);
+      });
+    });
+  }
+
+  record() {
+    this.audioChunks = [];
+    this.mediaRecorder.start(100)
+  }
+
+  stop() {
+    this.mediaRecorder.stop();
+    this.audioUrl = URL.createObjectURL(new Blob(this.audioChunks));
+  }
 }
