a19d231da1f35b93814405a21347d2456cb30a26
Anupam Samanta <anupam96786@gmail.com>
2023-05-18 01:08:18 +0530

    convert recorded audio to wav and post to backend

--- src/app/components/record-audio/record-audio.component.ts
+++ src/app/components/record-audio/record-audio.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {createFFmpeg} from '@ffmpeg/ffmpeg';
 
 @Component({
   selector: 'app-record-audio',
@@ -9,7 +10,6 @@ import {HttpClient} from "@angular/common/http";
 export class RecordAudioComponent {
   mediaRecorder!: MediaRecorder;
   audioChunks: Blob[] = [];
-  audioUrl?: string;
 
   constructor(private httpClient: HttpClient) {
     navigator.mediaDevices.getUserMedia({audio: true}).then(stream => {
@@ -27,16 +27,36 @@ export class RecordAudioComponent {
 
   record() {
     this.audioChunks = [];
-    this.mediaRecorder.start(100)
+    this.mediaRecorder.start(50)
   }
 
   stop() {
     this.mediaRecorder.stop();
-    let formData = new FormData();
-    let blobAudio = new Blob(this.audioChunks);
-    formData.append('audio', blobAudio);
-    this.httpClient.post('http://127.0.0.1:8000/', formData).subscribe((response) => {
-      console.log(response);
+    let blobWebm = new Blob(this.audioChunks);
+    this.convertWebmToWav(blobWebm).then(blobWav => {
+      let formData = new FormData();
+      formData.append('audio', blobWav);
+      this.httpClient.post('http://127.0.0.1:8000/', formData).subscribe((response) => {
+        console.log(response);
+      });
     });
+  }
+
+  async convertWebmToWav(webmBlob: Blob): Promise<Blob> {
+    const ffmpeg = createFFmpeg({
+      mainName: 'main',
+      corePath: 'https://unpkg.com/@ffmpeg/core-st@0.11.1/dist/ffmpeg-core.js',
+    });
+    await ffmpeg.load();
+
+    const inputName = 'input.webm';
+    const outputName = 'output.wav';
+
+    ffmpeg.FS('writeFile', inputName, new Uint8Array(await webmBlob.arrayBuffer()));
+
+    await ffmpeg.run('-i', inputName, outputName);
+
+    const outputData = ffmpeg.FS('readFile', outputName);
+    return new Blob([outputData.buffer], {type: 'audio/wav'});
   }
 }
